b41629552b3d620bafb87209e7c19b19ebe324a4
Callum Kirkwood <callum.kirkwood@gmail.com>
2018-06-04 12:53:25 +0100

    update OSC tool

--- controlPanel/oscTest.py
+++ controlPanel/oscTest.py
@@ -22,39 +22,21 @@ def oscInput(addr, tags, stuff, source):
   print addr, stuff, source
 
 # pot listener courtsey of Maxmillian Peters - https://stackoverflow.com/questions/37897483/how-to-display-a-changing-value-python
-def readPots():
-    pot1 = MCP3008(channel=0, device=0)
-    pot2 = MCP3008(channel=1, device=0)
-    pot3 = MCP3008(channel=2, device=0)
+def readPot(c):
+    pot = MCP3008(channel=c, device=0)
     threshold = 0.05
 
-    last_value_1 = (((pot1.value - 0) * (255 - 0)) / (1 - 0)) + 0
-    last_value_2 = (((pot2.value - 0) * (255 - 0)) / (1 - 0)) + 0
-    last_value_3 = (((pot3.value - 0) * (255 - 0)) / (1 - 0)) + 0
+    last_value = int((((pot.value - 0) * (255 - 0)) / (1 - 0)) + 0)
+
+    print(last_value)
 
     while True:
-
-        new_value_1 = (((pot1.value - 0) * (255 - 0)) / (1 - 0)) + 0
-        if abs((last_value_1 - new_value_1) / new_value_1) > threshold:
-            message1 = '{:.0f}'.format(new_value_1)
-            send_osc('/pot1', int(message1))
-	    print('pot1 = ', message1)
-        else:
-            sleep(0.05)
-
-        new_value_2 = (((pot2.value - 0) * (255 - 0)) / (1 - 0)) + 0
-        if abs((last_value_2 - new_value_2) / new_value_2) > threshold:
-            message2 = '{:.0f}'.format(new_value_2)
-            send_osc('/pot2', int(message2))
-            print('pot2 = ', message2)
-        else:
-            sleep(0.05)
-
-        new_value_3 = (((pot3.value - 0) * (255 - 0)) / (1 - 0)) + 0
-        if abs((last_value_3 - new_value_3) / new_value_3) > threshold:
-            message3 = '{:.0f}'.format(new_value_3)
-            send_osc('/pot3', int(message3))
-            print('pot3 = ', message3)
+        new_value = (((pot.value - 0) * (255 - 0)) / (1 - 0)) + 0
+        if abs((last_value - new_value) / new_value) > threshold:
+            message = int(new_value)
+            send_osc('/pot' + str(c), message)
+            print('Sending ', message, ' to pot #', str(c))
+            last_value = message
         else:
             sleep(0.05)
 
@@ -65,5 +47,6 @@ send_address = (client_ip, 8000)
 c = OSCClient()
 c.connect(send_address)
 
-
-readPots()
+readPot(0)
+readPot(1)
+readPot(2)
