f1a45c1d31a3036e076c6933069228c023bc272a
Callum Kirkwood <ck@callumkirkwood.com>
2018-06-04 12:47:00 +0100

    update

--- controlPanel/pottest.py
+++ controlPanel/pottest.py
@@ -2,22 +2,23 @@
 from time import sleep
 from gpiozero import MCP3008
 
-def readPot(c)
+def readPot(c):
     pot = MCP3008(channel=c, device=0)
     threshold = 0.05
 
-    last_value = (((pot.value - 0) * (255 - 0)) / (1 - 0)) + 0
+    last_value = int((((pot.value - 0) * (255 - 0)) / (1 - 0)) + 0)
 
     print(last_value)
 
     while True:
         new_value = (((pot.value - 0) * (255 - 0)) / (1 - 0)) + 0
         if abs((last_value - new_value) / new_value) > threshold:
-            print('{:.0f}'.format(new_value))
+            message = int(new_value)
             last_value = new_value
+            return message
         else:
             sleep(0.05)
 
-readPot(0)
-readPot(1)
-readPot(2)
+while True:
+    one = readPot(0)
+    print(one)
